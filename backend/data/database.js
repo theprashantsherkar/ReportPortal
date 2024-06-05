4a3a916f8894d1ce88432a5f52c3df0816969d52
Prashant <prashantsherkar7515400@gmail.com>
2024-06-06 00:05:27 +0530

    database connected

--- backend/data/database.js
+++ backend/data/database.js
@@ -1,8 +1,8 @@
 import mysql from 'mysql2';
 
 export var dbConnection = mysql.createConnection({
-    host: 'localhost',
+    host: "localhost",
     user: 'root',
-    password: 'Prashu@8484',
+    password: "Prashu@8484",
     database: "adminDb",
 }).promise()
